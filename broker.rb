39143c699f6b859a20da230993708c2efa23fcfa
Josh Nesbitt <josh@wearestac.com>
2016-05-16 14:14:14 +0100

    Update pusher.

--- broker.rb
+++ broker.rb
@@ -2,7 +2,14 @@ require 'twitter'
 require 'pusher'
 
 query = ENV['TWITTER_QUERY']
-client = Twitter::Streaming::Client.new do |config|
+
+$pusher = Pusher::Client.new(
+  app_id: ENV['PUSHER_APP_ID'],
+  key: ENV['PUSHER_KEY'],
+  secret: ENV['PUSHER_SECRET']
+)
+
+$client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
@@ -10,10 +17,10 @@ client = Twitter::Streaming::Client.new do |config|
 end
 
 def publish(tweet)
-  Pusher['main'].trigger('tweet:receive', tweet.attrs)
+  $pusher.trigger('main', 'tweet:receive', tweet.attrs)
 end
 
-client.filter(:track => query) do |object|
+$client.filter(:track => query) do |object|
 
   begin
     case object
